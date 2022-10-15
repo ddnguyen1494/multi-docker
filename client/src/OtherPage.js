434dc8897663c8f17970ce836c154e2cfd619ddb
ddnguyen1494 <ddnguyen1494@gmail.com>
2022-10-15 14:50:53 -0700

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  { Link } from 'react-router-dom';
+
+const otherPage = () => {
+    return (
+    <div>
+        In some other page!
+        <Link to="/">Go back home</Link>
+    </div>
+    );
+};
+
+export default otherPage;
+
